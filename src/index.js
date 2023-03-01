f641b5c44e634dee814fb2060b45ea57ca09dfbd
REDT <87268805+RafaelTorres0-0@users.noreply.github.com>
2023-03-01 10:29:20 -0400

    creating navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Esta vivo!!')
+import './style.css';
+import logo from './img/FridaLogo.png'
+
+const content =  document.querySelector('.content');
+
+const navBar = () => {
+   const img = document.createElement('img')
+   img.src = logo;
+   return img;
+}
+
+content.appendChild(navBar())
