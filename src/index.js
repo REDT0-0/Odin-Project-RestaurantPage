59b6623c9d28117837dab0f14ed7de919e2804e2
REDT <87268805+RafaelTorres0-0@users.noreply.github.com>
2023-03-02 10:33:39 -0400

    almost done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
 import logoFrida from './img/FridaLogo.png'
-
+import burguerImg from './img/burger.jpg'
+import chilaquilesImg from './img/chilaquiles.jpg'
+import fajitasImg from'./img/fajitas.jpg'
+import quesadillaImg from './img/quesadilla.jpg'
+import tacoImg from './img/taco.jpg'
+import salchipapasImg from './img/salchipapas.jpg'
 
 const content = document.querySelector('.content');
 
@@ -68,26 +73,32 @@ const home = () => {
 const menu = () => {
   const plates = {
     burger: {
+      img: burguerImg,
       price: '8',
       details: 'Ground beef or other meat (such as turkey, chicken, or veggie patties), Salt and pepper, Buns, Lettuce, Tomato, Onion, Cheese, Ketchup, Mustard, Mayonnaise, Pickles',
     },
     chilaquiles: {
+      img: chilaquilesImg,
       price: '8',
       details: 'Corn tortillas Vegetable oil or cooking spray, Red or green salsa or sauce (made with tomatoes, tomatillos, chilies, and other seasonings), Onion, Garlic, Salt, Queso fresco or other cheese, Crema or sour cream, Avocado, Cilantro, Some variations of Chilaquiles may also include chicken, chorizo, or eggs as additional toppings. The exact ingredients used can be adjusted to taste, and there are many ways to customize the dish.',
     },
     fajitas: {
+      img: fajitasImg,
       price: '6.5',
       details: 'Meat (usually beef, chicken, or shrimp), Bell peppers (usually red, green, and/or yellow), Onion, Garlic, Lime juice, Olive oil, Chili powder, Cumin, Salt and pepper, Flour or corn tortillas, Toppings such as sour cream, guacamole, shredded cheese, and salsa.',
     },
     quesadilla: {
+      img: quesadillaImg,
       price: '12',
       details: 'Flour or corn tortillas, Shredded cheese (such as cheddar, Monterrey Jack, or queso blanco), Optional fillings such as cooked chicken, steak, shrimp, beans, vegetables (such as peppers, onions, or mushrooms), or salsa',
     },
     taco: {
+      img: tacoImg,
       price: '9',
       details: 'Corn or flour tortillas, Meat (such as beef, chicken, pork, or fish) or beans for a vegetarian option, Seasonings for the meat (such as cumin, chili powder, or garlic), Vegetables (such as lettuce, tomatoes, onions, or peppers), Cheese (such as queso fresco, cheddar, or Monterey Jack), Salsa or hot sauce, Sour cream or guacamole',
     },
     salchipapas: {
+      img: salchipapasImg,
       price: '10',
       details: 'French fries (papas fritas), either homemade or store-bought, Sausages (salchichas), sliced or diced and fried, Salsa golf, a sauce made with ketchup and mayonnaise, or other sauces such as chimichurri, garlic aioli, or hot sauce, Optional toppings such as diced onions, tomatoes, or peppers, Salt and pepper',
     }
@@ -95,13 +106,39 @@ const menu = () => {
 
   const menuContainer = document.createElement('div');
   menuContainer.classList.add('menu-container');
-  menuContainer.textContent="RAFA"; 
 
   for (let plate in plates) {
     if (plates.hasOwnProperty(plate)) {
-      console.log('Tipo de plato: ' + plate);
-      console.log('Precio: ' + plates[plate].price);
-      console.log('Detalles: ' + plates[plate].details);
+      const foodItemDiv = document.createElement('div');
+      foodItemDiv.classList.add('food-items');
+
+      const foodImg = document.createElement('img');
+      foodImg.src = plates[plate].img;
+
+      const detailsDiv = document.createElement('div');
+      detailsDiv.classList.add('details');
+
+      const detailsSubDiv = document.createElement('div');
+      detailsSubDiv.classList.add('details-sub');
+
+      const foodName = document.createElement('h5');
+      foodName.textContent = plate.replace(/-/g, ' ');
+
+      const foodPrice = document.createElement('h5');
+      foodPrice.classList.add('price');
+      foodPrice.textContent = `$${plates[plate].price}`;
+
+      detailsSubDiv.appendChild(foodName);
+      detailsSubDiv.appendChild(foodPrice);
+
+      const foodDetails = document.createElement('p');
+      foodDetails.textContent = plates[plate].details.replace(/\n\s+/g, '\n');
+
+      detailsDiv.appendChild(foodImg);
+      detailsDiv.appendChild(detailsSubDiv);
+      detailsDiv.appendChild(foodDetails);
+
+      menuContainer.appendChild(detailsDiv);
     }
   }
 
